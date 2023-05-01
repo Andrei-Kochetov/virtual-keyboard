b758d33396816a766adcdbbb4d7d82c81555746e
Andrei <ai.dr@yandex.ru>
2023-05-01 21:39:32 +0400

    refactor: fix eslint mistake

--- src/style.js
+++ src/style.js
@@ -2,11 +2,10 @@ import './sass/main.scss';
 import { createTextArea } from './js/textarea/textaria';
 import { createKeyBoard } from './js/keyboard/keyboard';
 
-
 createTextArea();
 createKeyBoard();
 
-let description = document.createElement('p');
+const description = document.createElement('p');
 description.innerHTML = 'Клавиатура создана в операционной системе Windows <br> Для переключения языка комбинация: левыe CTRL + ALT';
 description.style.textAlign = 'center';
 document.body.appendChild(description);
